77a99cc24a219197c2cced9e15dab37656730899
Christoph Otter <christophotter@gmail.com>
2016-11-25 15:57:08 +0100

    Refactor folder names

---
+++ Plugins/YeomanTemplates/Yeoman/GeneratorListParser.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using YeomanTemplates.Helper;
+
+namespace YeomanTemplates.yeoman
+{
+    class GeneratorListParser
+    {
+        public readonly NestedStringList Result;
+        private NestedStringList lastMainGen;
+
+        public GeneratorListParser()
+        {
+            Result = new NestedStringList(null);
+        }
+
+        public void ParseInput(StreamReader reader)
+        {
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                ParseLine(line);
+            }
+        }
+
+        /// <summary>
+        /// Parses the given line into a TreeNode and adds it to the given TreeView.
+        /// The output has to be parsed from top to bottom
+        /// </summary>
+        /// <param name="line"></param>
+        private void ParseLine(string line)
+        {
+            if (line.StartsWith("    ")) //sub-generator
+            {
+                if (lastMainGen == null)
+                {
+                    PluginCore.Managers.ErrorManager.ShowError(new Exception("Error reading yo output: " + line));
+                    return;
+                }
+
+                var node = new NestedStringList(line.Substring(4));
+                lastMainGen.Add(node);
+            }
+            else if (line.StartsWith("  ")) //generator
+            {
+                var node = new NestedStringList(line.Substring(2));
+                lastMainGen = node;
+                Result.Add(node);
+            }
+            //else ignore
+        }
+    }
+}
---
+++ Plugins/YeomanTemplates/Yeoman/YoHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using YeomanTemplates.Helper;
+
+namespace YeomanTemplates.yeoman
+{
+    class YoHelper
+    {
+        public static NestedStringList GetYoGenerators(string yoCmd)
+        {
+            try
+            {
+                var yo = GetYoProcess(yoCmd);
+                yo.StartInfo.Arguments = "--generators";
+                yo.Start();
+
+                var reader = yo.StandardOutput;
+
+                var parser = new yeoman.GeneratorListParser();
+                parser.ParseInput(reader);
+
+                reader.Dispose();
+
+                return parser.Result;
+            }
+            catch (Exception e)
+            {
+                PluginCore.Managers.ErrorManager.ShowError("Error running yo, please check if the command in the settings is correct: " + yoCmd, e);
+            }
+
+            return null;
+        }
+
+        public static Process GetYoProcess(string yoCmd)
+        {
+            var proc = new Process();
+            proc.StartInfo.FileName = yoCmd;
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.RedirectStandardError = true;
+            proc.StartInfo.CreateNoWindow = true;
+            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            proc.EnableRaisingEvents = true;
+
+            return proc;
+        }
+
+        public static Process GetVisibleYo(string yoCmd)
+        {
+            var proc = new Process();
+            proc.StartInfo.FileName = yoCmd;
+            proc.StartInfo.UseShellExecute = true;
+            proc.StartInfo.CreateNoWindow = false;
+
+            return proc;
+        }
+
+        public static string GetYoFile(string folder)
+        {
+            return Path.Combine(folder, "yo.cmd");
+        }
+    }
+}
